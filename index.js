a69ee60904327d0cc34cbe7acf7e76bd8325abb1
daniela-idara <purpleelephant1987@gmail.com>
2019-07-25 23:16:37 -0500

    complete part one

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static("public"));
 
-app.get('/', function (req, res){
-  res.json({name: "Daniela", age:32, color:"purple"})
-})
-
 // Create express routes to get all things
 // response.json() the appropriate array
 
@@ -21,6 +17,24 @@ app.get('/', function (req, res){
 //add a path variable for id
 //use the params.id to .find() the item from the appropriate array
 
+//comments
+app.get('/comments', (req, res)=>{
+  res.json(comments)
+})
+
+app.get('/comments/:id', (req, res) => {
+  let comment = comments.find(p=>p._id === Number(req.params.id));
+  res.json(comment)
+})
+
+app.post('/comments/', (req, res) => {
+  let id = comments.length + 1;
+  let body = req.body.body;
+  let postId = req.body.postId;
+  comments.push({"_id":id, "body":body, "postId":postId})
+  res.json(comments);
+})
+
 //contacts
 app.get('/contacts', (req, res)=>{
   res.json(contacts)
@@ -28,6 +42,15 @@ app.get('/contacts', (req, res)=>{
 app.get('/contacts/:id', (req, res) => {
   let contact = contacts.find(p=>p._id == Number(req.params.id));
   res.json(contact)
+})
+
+app.post('/contacts/', (req, res) => {
+  let id = contacts.length + 1;
+  let name = req.body.name;
+  let occupation = req.body.occupation;
+  let avatar = req.body.avatar;
+  contacts.push({"_id":id, "name":name, "occupation":occupation, "avatar":avatar})
+  res.json(contacts);
 })
 
 //vehicles
@@ -40,14 +63,13 @@ app.get('/vehicles/:id', (req, res) => {
   res.json(vehicle)
 })
 
-//comments
-app.get('/comments', (req, res)=>{
-  res.json(comments)
-})
-
-app.get('/comments/:id', (req, res) => {
-  let comment = comments.find(p=>p._id === Number(req.params.id));
-  res.json(comment)
+app.post('/vehicles/', (req, res) => {
+  let id = vehicles.length + 1;
+  let year = req.body.year;
+  let make = req.body.make;
+  let model = req.body.model;
+  vehicles.push({year, make, model, "_id":id, "Vehicle Year":year, "Vehicle Make":make, "Vehicle Model":model})
+  res.json(vehicles);
 })
 
 //products
@@ -58,6 +80,14 @@ app.get('/products', (req, res)=>{
 app.get('/products/:id', (req, res) => {
   let product = products.find(p=>p._id === Number(req.params.id));
   res.json(product)
+})
+
+app.post('/products/', (req, res) => {
+  let id = products.length + 1;
+  let name = req.body.name;
+  let description = req.body.description;
+  products.push({name, description, "_id":id, "Product Name":name, "Product desc":description})
+  res.json(products);
 })
 
 
